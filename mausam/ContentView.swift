c08a7ca8e402483e54a7f25acee8e5bb81c12c6f
sanjana-shah <sanjanashah20211@gmail.com>
2026-07-25 11:21:30 -0400

    Initial Commit

---
+++ mausam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mausam
+//
+//  Created by Sanjana Shah on 7/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mausam/mausamApp.swift
@@ -0,0 +1,17 @@
+//
+//  mausamApp.swift
+//  mausam
+//
+//  Created by Sanjana Shah on 7/25/26.
+//
+
+import SwiftUI
+
+@main
+struct mausamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
